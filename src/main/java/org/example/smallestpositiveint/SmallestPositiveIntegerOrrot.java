30508032539d2a06c00f4ddfcca5cca2f984a1c0
Orlando Rodriguez <sort465@gmail.com>
2019-12-07 12:32:49 -0500

    Orlando First Solution (Not using Streams))
    This is a solution for the exercise without using streams.

--- src/main/java/org/example/smallestpositiveint/SmallestPositiveIntegerOrrot.java
+++ src/main/java/org/example/smallestpositiveint/SmallestPositiveIntegerOrrot.java
@@ -2,13 +2,16 @@ package org.example.smallestpositiveint;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SmallestPositiveIntegerOrrot {
 
-    /*
+    public static final int DEFAULT_CAPACITY = 10;
+
+    /**
     Write a function: def solution(A)
     that, given an array A of N integers returns the smallest positive integer (greater than 0) that does
     not occur in A.
@@ -17,7 +20,7 @@ public class SmallestPositiveIntegerOrrot {
     Given A • the f should return 1 .
     Write an efficient algorithm for the following N is an integer within the range (1..100,
     each element of array A is an integer within the range (-100.000 to 100.000l.
-     */
+    **/
     public static Integer findSmallestPositiveInteger(Integer[] integers) {
 
         List<Integer> numbers = getSortedDistinctIntegers(integers);
@@ -39,26 +42,68 @@ public class SmallestPositiveIntegerOrrot {
         return findNextPositiveValue(numbers.get(numbers.size() - 1));
     }
 
+    /**
+     * Returns a List of the integers, excluding the negative values and always starting from 0.
+     * @param integers
+     * @return
+     */
     private static List<Integer> getSortedDistinctIntegers(Integer[] integers) {
 
+        /*
+        Implementation with Streams!
+
         return Arrays.stream(integers)
+
                 .filter(SmallestPositiveIntegerOrrot::isPositive)
                 .distinct()
                 .sorted()
                 .collect(Collectors.toCollection(SmallestPositiveIntegerOrrot::buildListWithZero));
+         */
+        List<Integer> integerList = buildListWithZero();
+        for (int i = 0; i < integers.length; i++) {
+
+            int current = integers[i];
+            if (isPositive(current)) {
+                for (int j = 0; j < integerList.size(); j++) {
+
+                    if (current < integerList.get(j)) {
+                        integerList.add(j, current);
+                        break;
+                    } else if (current == integerList.get(j)) {
+                        break;
+                    }
+                }
+                integerList.add(current);
+            }
+        }
+        return integerList;
     }
 
+    /**
+     * Checks if the value is a positive number.
+     * @param value
+     * @return
+     */
     private static boolean isPositive(Integer value) {
         return value > 0;
     }
 
+    /**
+     * Creates a list of integers with a 0 in the first index.
+     * @return
+     */
     private static List<Integer> buildListWithZero() {
         // Setting an initial capacity will be better!
-        List<Integer> initialList = new ArrayList<>();
+        List<Integer> initialList = new LinkedList<>();
         initialList.add(0);
         return initialList;
     }
 
+    /**
+     * Finds the next value for the number. If the number is negative, returns 1.
+     * @param number
+     * @return
+     */
     private static Integer findNextPositiveValue(Integer number) {
         if (number + 1 > 0) {
             return number + 1;
